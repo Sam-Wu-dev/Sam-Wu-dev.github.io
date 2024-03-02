571ec7f4cd45792d775394b5de58774886723e0d
Sam-Wu-dev <sam2014521@gmail.com>
2024-03-02 17:10:55 +0800

    update web

--- dist/assets/小星星20230401-764e2608.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/spectogram-90955b38.jpg",t="/assets/onsetframes-d89b992b.jpg",a="/assets/fundamentalfrequency-0a9c169f.jpg",e="/assets/onsetasmask-e4725633.jpg",o="/assets/result-812cdbe8.jpg",p="/assets/小星星20230401-91eb4bea.mp3",_="/assets/小星星20230401-44758b18.wav";export{s as _,t as a,a as b,e as c,o as d,p as e,_ as f};
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,33 +6,34 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../components/HomePage.vue')
+      component: () => import('../components/HomePage.vue'),
+      meta: { title: 'Sam Wu' }
     },
     {
       path: '/arduino',
       name: 'arduino',
-      component: () => import('../components/projects/ArduinoMusicBox.vue')
+      component: () => import('../components/projects/ArduinoMusicBox.vue'),
+      meta: { title: 'Sam Wu' }
     },
     {
       path: '/ninjastrikemaster',
       name: 'ninjastrikemaster',
-      component: () => import('../components/projects/NinjaStrikeMaster.vue')
+      component: () => import('../components/projects/NinjaStrikeMaster.vue'),
+      meta: { title: 'Sam Wu' }
     },
     {
       path: '/licenseplaterecognition',
       name: 'licenseplaterecognition',
-      component: () => import('../components/projects/licenseplaterecognition.vue')
+      component: () => import('../components/projects/licenseplaterecognition.vue'),
+      meta: { title: 'Sam Wu' }
     },
     {
       path: '/opengl',
       name: 'opengl',
-      component: () => import('../components/projects/OpenGL.vue')
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: () => import('../components/projects/TestPage.vue')
-    },{ path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('../components/HomePage.vue') }
+      component: () => import('../components/projects/OpenGL.vue'),
+      meta: { title: 'Sam Wu' }
+    },{ path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('../components/HomePage.vue'),
+    meta: { title: 'Sam Wu' } }
   ]
 })
 
